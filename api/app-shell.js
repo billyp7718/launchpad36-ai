22525596853c0af70cef88de5e6bf0a55cccf3d3
billyp7718 <wtpantaleo@gmail.com>
2026-09-08 19:44:59 -0400

    Load robust Deep Buyer Search controls

--- api/app-shell.js
+++ api/app-shell.js
@@ -12,6 +12,7 @@ export default function handler(req,res){
     const routeFitUiPath=path.join(root,'route-fit-ui.js');
     const buyerRetryUiPath=path.join(root,'buyer-retry-ui.js');
     const deepBuyerUiPath=path.join(root,'deep-buyer-search-ui.js');
+    const deepBuyerUiV2Path=path.join(root,'deep-buyer-search-ui-v2.js');
     let html=fs.readFileSync(htmlPath,'utf8');
     const channelUi=fs.readFileSync(channelUiPath,'utf8');
     const buyerUi=fs.readFileSync(buyerUiPath,'utf8');
@@ -20,7 +21,8 @@ export default function handler(req,res){
     const routeFitUi=fs.readFileSync(routeFitUiPath,'utf8');
     const buyerRetryUi=fs.readFileSync(buyerRetryUiPath,'utf8');
     const deepBuyerUi=fs.readFileSync(deepBuyerUiPath,'utf8');
-    html=html.replace('</body>',`<script>${channelUi}</script><script>${buyerUi}</script><script>${workflowUi}</script><script>${buyerCoverageUi}</script><script>${routeFitUi}</script><script>${buyerRetryUi}</script><script>${deepBuyerUi}</script></body>`);
+    const deepBuyerUiV2=fs.readFileSync(deepBuyerUiV2Path,'utf8');
+    html=html.replace('</body>',`<script>${channelUi}</script><script>${buyerUi}</script><script>${workflowUi}</script><script>${buyerCoverageUi}</script><script>${routeFitUi}</script><script>${buyerRetryUi}</script><script>${deepBuyerUi}</script><script>${deepBuyerUiV2}</script></body>`);
     res.setHeader('content-type','text/html; charset=utf-8');
     res.setHeader('cache-control','no-store, max-age=0');
     return res.status(200).send(html);
