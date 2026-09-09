fb3a68fb8e30f6fa56556dfdc3a48738d007bc01
billyp7718 <wtpantaleo@gmail.com>
2026-09-08 20:12:56 -0400

    Escape injected script closers in app shell

--- api/app-shell.js
+++ api/app-shell.js
@@ -1,6 +1,8 @@
 import fs from 'fs';
 import path from 'path';
 
+const safeScript = source => String(source || '').replace(/<\/script/gi, '<\\/script');
+
 export default function handler(req,res){
   try{
     const root=process.cwd();
@@ -15,16 +17,19 @@ export default function handler(req,res){
     const deepBuyerUiV2Path=path.join(root,'deep-buyer-search-ui-v2.js');
     const deepBuyerUiV3Path=path.join(root,'deep-buyer-search-ui-v3.js');
     let html=fs.readFileSync(htmlPath,'utf8');
-    const channelUi=fs.readFileSync(channelUiPath,'utf8');
-    const buyerUi=fs.readFileSync(buyerUiPath,'utf8');
-    const workflowUi=fs.readFileSync(workflowUiPath,'utf8');
-    const buyerCoverageUi=fs.readFileSync(buyerCoverageUiPath,'utf8');
-    const routeFitUi=fs.readFileSync(routeFitUiPath,'utf8');
-    const buyerRetryUi=fs.readFileSync(buyerRetryUiPath,'utf8');
-    const deepBuyerUi=fs.readFileSync(deepBuyerUiPath,'utf8');
-    const deepBuyerUiV2=fs.readFileSync(deepBuyerUiV2Path,'utf8');
-    const deepBuyerUiV3=fs.readFileSync(deepBuyerUiV3Path,'utf8');
-    html=html.replace('</body>',`<script>${channelUi}</script><script>${buyerUi}</script><script>${workflowUi}</script><script>${buyerCoverageUi}</script><script>${routeFitUi}</script><script>${buyerRetryUi}</script><script>${deepBuyerUi}</script><script>${deepBuyerUiV2}</script><script>${deepBuyerUiV3}</script></body>`);
+    const scripts=[
+      channelUiPath,
+      buyerUiPath,
+      workflowUiPath,
+      buyerCoverageUiPath,
+      routeFitUiPath,
+      buyerRetryUiPath,
+      deepBuyerUiPath,
+      deepBuyerUiV2Path,
+      deepBuyerUiV3Path
+    ].map(file=>safeScript(fs.readFileSync(file,'utf8')));
+    const injected=scripts.map(source=>`<script>${source}</script>`).join('');
+    html=html.replace('</body>',`${injected}</body>`);
     res.setHeader('content-type','text/html; charset=utf-8');
     res.setHeader('cache-control','no-store, max-age=0');
     return res.status(200).send(html);
